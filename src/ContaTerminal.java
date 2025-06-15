cb964eaa17aefecb754b2695b80de4b3176ab1d5
EdevaldoVitor <vitor.cohene@hotmail.com>
2025-06-15 16:13:42 -0300

    Feat: Implementa solução na classe ContaTerminal para o desafio proposto.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+
+        String agencia;
+        String nomeCliente;
+        int numero;
+        double saldo;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Por favor, Digite o seu nome:");
+        nomeCliente = input.next();
+        System.out.print("Por favor, Digite o numero da conta: ");
+        numero = input.nextInt();
+        System.out.print("Por favor, Digite o numero da agência: ");
+        agencia = input.next();
+        System.out.print("Por favor, Digite o saldo do conta: ");
+        saldo = input.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " " +
+                "e seu saldo " + saldo + " já está disponível para saque.");
 
     }
 }
